39e967ea5601f22f184522938fc769b5a00f8b0b
jtm <joetymatthews@gmail.com>
2021-06-28 21:48:01 +0100

    new: add decoder & encoder for webflux

--- src/main/kotlin/com/jtm/minecraft/MinecraftService.kt
+++ src/main/kotlin/com/jtm/minecraft/MinecraftService.kt
@@ -1,9 +1,14 @@
 package com.jtm.minecraft
 
+import feign.codec.Decoder
+import feign.codec.Encoder
+import feign.jackson.JacksonDecoder
+import feign.jackson.JacksonEncoder
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.context.annotation.Bean
 
 @EnableFeignClients
 @EnableDiscoveryClient
@@ -12,4 +17,14 @@ open class MinecraftService
 
 fun main(args: Array<String>) {
     SpringApplication.run(MinecraftService::class.java, *args)
+}
+
+@Bean
+fun decoder(): Decoder {
+    return JacksonDecoder()
+}
+
+@Bean
+fun encoder(): Encoder {
+    return JacksonEncoder()
 }
