cabef6ff5bf5b54b35dbb2148a6505497f02e5ab
Joe Matthews <joetymatthews@gmail.com>
2021-06-29 21:24:17 +0100

    refactor: normal feign test

--- src/main/kotlin/com/jtm/minecraft/data/service/TestService.kt
+++ src/main/kotlin/com/jtm/minecraft/data/service/TestService.kt
@@ -1,6 +1,8 @@
 package com.jtm.minecraft.data.service
 
+import com.jtm.minecraft.core.domain.exceptions.PluginNotFound
 import com.jtm.minecraft.core.domain.model.AccountInfo
+import com.jtm.minecraft.core.usecase.repository.PluginRepository
 import com.jtm.minecraft.data.proxy.AccountProxy
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Lazy
@@ -8,9 +10,12 @@ import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 
 @Service
-class TestService @Autowired constructor(@Lazy private val accountProxy: AccountProxy) {
+class TestService @Autowired constructor(private val pluginRepository: PluginRepository, @Lazy private val accountProxy: AccountProxy) {
 
     fun testAccount(bearer: String): Mono<AccountInfo> {
-        return accountProxy.getAccount(bearer)
+        val account = accountProxy.getAccount(bearer)
+        println("${account.id}")
+        return pluginRepository.findById(account.id)
+            .switchIfEmpty(Mono.defer { Mono.error(PluginNotFound()) }).cast(AccountInfo::class.java)
     }
 }
