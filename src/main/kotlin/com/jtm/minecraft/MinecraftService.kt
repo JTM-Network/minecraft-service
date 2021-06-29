c87533b42d2607be235a294e02828b05a810cbed
Joe Matthews <joetymatthews@gmail.com>
2021-06-29 21:26:38 +0100

    refactor: normal feign test

--- src/main/kotlin/com/jtm/minecraft/MinecraftService.kt
+++ src/main/kotlin/com/jtm/minecraft/MinecraftService.kt
@@ -1,11 +1,12 @@
 package com.jtm.minecraft
 
+import com.jtm.minecraft.data.proxy.AccountProxy
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
-import reactivefeign.spring.config.EnableReactiveFeignClients
+import org.springframework.cloud.openfeign.EnableFeignClients
 
-@EnableReactiveFeignClients
+@EnableFeignClients(clients = [AccountProxy::class])
 @EnableDiscoveryClient
 @SpringBootApplication
 open class MinecraftService
