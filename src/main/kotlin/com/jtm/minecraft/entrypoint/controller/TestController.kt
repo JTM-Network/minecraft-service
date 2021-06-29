937f67d3a64edd81210d8f7d8ca78841e314abd6
Joe Matthews <joetymatthews@gmail.com>
2021-06-29 23:09:50 +0100

    new: test account id retrieving from jwt token

---
+++ src/main/kotlin/com/jtm/minecraft/entrypoint/controller/TestController.kt
@@ -0,0 +1,22 @@
+package com.jtm.minecraft.entrypoint.controller
+
+import com.jtm.minecraft.core.usecase.token.AccountTokenProvider
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.server.reactive.ServerHttpRequest
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+import java.util.*
+
+@RestController
+@RequestMapping("/test")
+class TestController @Autowired constructor(private val accountTokenProvider: AccountTokenProvider) {
+
+    @GetMapping("/account")
+    fun account(request: ServerHttpRequest): Mono<UUID> {
+        val bearer = request.headers.getFirst("Authorization")
+        val token = accountTokenProvider.resolveToken(bearer)
+        return Mono.just(accountTokenProvider.getAccountId(token))
+    }
+}
