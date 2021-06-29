f3e12f374f231e9448b08a008404fd24830f3290
Joe Matthews <joetymatthews@gmail.com>
2021-06-29 18:11:20 +0100

    refactor: update build gradle

--- src/main/kotlin/com/jtm/minecraft/MinecraftService.kt
+++ src/main/kotlin/com/jtm/minecraft/MinecraftService.kt
@@ -1,14 +1,9 @@
 package com.jtm.minecraft
 
-import feign.codec.Decoder
-import feign.codec.Encoder
-import feign.jackson.JacksonDecoder
-import feign.jackson.JacksonEncoder
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.openfeign.EnableFeignClients
-import org.springframework.context.annotation.Bean
 
 @EnableFeignClients
 @EnableDiscoveryClient
