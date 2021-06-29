e1d056c39a8f65268c2a131f7487646b70d97906
Joe Matthews <joetymatthews@gmail.com>
2021-06-29 19:09:18 +0100

    fix: fail on empty beans

--- src/main/kotlin/com/jtm/minecraft/entrypoint/configuration/FeignConfiguration.kt
+++ src/main/kotlin/com/jtm/minecraft/entrypoint/configuration/FeignConfiguration.kt
@@ -1,11 +1,14 @@
 package com.jtm.minecraft.entrypoint.configuration
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
 import feign.codec.Decoder
 import feign.codec.Encoder
 import feign.jackson.JacksonDecoder
 import feign.jackson.JacksonEncoder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 
 @Configuration
 open class FeignConfiguration {
@@ -18,5 +21,13 @@ open class FeignConfiguration {
     @Bean
     open fun encoder(): Encoder {
         return JacksonEncoder()
+    }
+
+    @Bean
+    open fun mappingJackson2HttpMessageConverter(): MappingJackson2HttpMessageConverter {
+        val mapper = ObjectMapper()
+        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        val converter = MappingJackson2HttpMessageConverter(mapper);
+        return converter;
     }
 }
