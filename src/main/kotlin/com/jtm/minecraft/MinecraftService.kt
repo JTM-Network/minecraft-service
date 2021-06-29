5dc0b00f90eb4001a95ad14b38144f1d09214f62
Joe Matthews <joetymatthews@gmail.com>
2021-06-29 22:45:56 +0100

    new: test account id retrieving from jwt token

--- src/main/kotlin/com/jtm/minecraft/MinecraftService.kt
+++ src/main/kotlin/com/jtm/minecraft/MinecraftService.kt
@@ -1,12 +1,9 @@
 package com.jtm.minecraft
 
-import com.jtm.minecraft.data.proxy.AccountProxy
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
-import org.springframework.cloud.openfeign.EnableFeignClients
 
-@EnableFeignClients(clients = [AccountProxy::class])
 @EnableDiscoveryClient
 @SpringBootApplication
 open class MinecraftService
--- src/main/kotlin/com/jtm/minecraft/core/domain/exceptions/InvalidHeader.kt
+++
@@ -1,9 +0,0 @@
-package com.jtm.minecraft.core.domain.exceptions
-
-import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.ResponseStatus
-import java.lang.RuntimeException
-
-@ResponseStatus(value = HttpStatus.UNAUTHORIZED, reason = "Invalid header given.")
-class InvalidHeader: RuntimeException() {
-}
