06ea76729f742a228057decbdba056efd300d4a1
Joe Matthews <joetymatthews@gmail.com>
2021-08-19 19:50:55 +0100

    refactor: fix typo

--- src/main/kotlin/com/jtm/minecraft/core/domain/exceptions/profile/ProfileNotFound.kt
+++ src/main/kotlin/com/jtm/minecraft/core/domain/exceptions/profile/ProfileNotFound.kt
@@ -4,5 +4,5 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
 import java.lang.RuntimeException
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Plugin not found.")
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Profile not found.")
 class ProfileNotFound: RuntimeException()
