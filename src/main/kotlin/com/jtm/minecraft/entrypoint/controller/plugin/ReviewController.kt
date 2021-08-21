24739da011e48888f87c9f6cf94669d49ab74d04
Joe Matthews <joetymatthews@gmail.com>
2021-08-21 20:50:13 +0100

    new: add plugin review endpoint w/ tests

---
+++ src/main/kotlin/com/jtm/minecraft/entrypoint/controller/plugin/ReviewController.kt
@@ -0,0 +1,46 @@
+package com.jtm.minecraft.entrypoint.controller.plugin
+
+import com.jtm.minecraft.core.domain.dto.PluginReviewDto
+import com.jtm.minecraft.core.domain.entity.plugin.PluginReview
+import com.jtm.minecraft.data.service.plugin.ReviewService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.server.reactive.ServerHttpRequest
+import org.springframework.web.bind.annotation.*
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import java.util.*
+
+@RestController
+@RequestMapping("/review")
+class ReviewController @Autowired constructor(private val reviewService: ReviewService) {
+
+    @PostMapping
+    fun postReview(@RequestBody dto: PluginReviewDto, request: ServerHttpRequest): Mono<PluginReview> {
+        return reviewService.insertReview(dto, request)
+    }
+
+    @PutMapping
+    fun putReview(@RequestBody dto: PluginReviewDto, request: ServerHttpRequest): Mono<PluginReview> {
+        return reviewService.updateReview(dto, request)
+    }
+
+    @GetMapping("/{id}")
+    fun getReview(@PathVariable id: UUID): Mono<PluginReview> {
+        return reviewService.getReview(id)
+    }
+
+    @GetMapping("/plugin/{pluginId}")
+    fun getReviewByPluginId(@PathVariable pluginId: UUID): Flux<PluginReview> {
+        return reviewService.getReviewByPlugin(pluginId)
+    }
+
+    @GetMapping("/all")
+    fun getReviews(): Flux<PluginReview> {
+        return reviewService.getReviews()
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteReview(@PathVariable id: UUID): Mono<PluginReview> {
+        return reviewService.removeReview(id)
+    }
+}
---
+++ src/test/kotlin/com/jtm/minecraft/entrypoint/controller/plugin/ReviewControllerTest.kt
@@ -0,0 +1,147 @@
+package com.jtm.minecraft.entrypoint.controller.plugin
+
+import com.jtm.minecraft.core.domain.dto.PluginReviewDto
+import com.jtm.minecraft.core.domain.entity.plugin.PluginReview
+import com.jtm.minecraft.data.service.plugin.ReviewService
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito.*
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.anyOrNull
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.verifyNoMoreInteractions
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.reactive.server.WebTestClient
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import java.util.*
+
+@RunWith(SpringRunner::class)
+@WebFluxTest(ReviewController::class, excludeAutoConfiguration = [ReactiveSecurityAutoConfiguration::class])
+@AutoConfigureWebTestClient
+class ReviewControllerTest {
+
+    @Autowired
+    lateinit var testClient: WebTestClient
+
+    @MockBean
+    lateinit var reviewService: ReviewService
+
+    private val dto = PluginReviewDto(UUID.randomUUID(), 3.5, "Review comment")
+    private val created = PluginReview(accountId = UUID.randomUUID(), pluginId = UUID.randomUUID(), rating = 3.5, comment = "Review comment.")
+
+    @Test
+    fun postReviewTest() {
+        `when`(reviewService.insertReview(anyOrNull(), anyOrNull())).thenReturn(Mono.just(created))
+
+        testClient.post()
+            .uri("/review")
+            .bodyValue(dto)
+            .exchange()
+            .expectStatus().isOk
+            .expectBody()
+            .jsonPath("$.accountId").isEqualTo(created.accountId.toString())
+            .jsonPath("$.pluginId").isEqualTo(created.pluginId.toString())
+            .jsonPath("$.rating").isEqualTo(created.rating)
+            .jsonPath("$.comment").isEqualTo(created.comment)
+
+        verify(reviewService, times(1)).insertReview(anyOrNull(), anyOrNull())
+        verifyNoMoreInteractions(reviewService)
+    }
+
+    @Test
+    fun putReviewTest() {
+        `when`(reviewService.updateReview(anyOrNull(), anyOrNull())).thenReturn(Mono.just(created))
+
+        testClient.put()
+            .uri("/review")
+            .bodyValue(dto)
+            .exchange()
+            .expectStatus().isOk
+            .expectBody()
+            .jsonPath("$.accountId").isEqualTo(created.accountId.toString())
+            .jsonPath("$.pluginId").isEqualTo(created.pluginId.toString())
+            .jsonPath("$.rating").isEqualTo(created.rating)
+            .jsonPath("$.comment").isEqualTo(created.comment)
+
+        verify(reviewService, times(1)).updateReview(anyOrNull(), anyOrNull())
+        verifyNoMoreInteractions(reviewService)
+    }
+
+    @Test
+    fun getReviewTest() {
+        `when`(reviewService.getReview(anyOrNull())).thenReturn(Mono.just(created))
+
+        testClient.get()
+            .uri("/review/${UUID.randomUUID()}")
+            .exchange()
+            .expectStatus().isOk
+            .expectBody()
+            .jsonPath("$.accountId").isEqualTo(created.accountId.toString())
+            .jsonPath("$.pluginId").isEqualTo(created.pluginId.toString())
+            .jsonPath("$.rating").isEqualTo(created.rating)
+            .jsonPath("$.comment").isEqualTo(created.comment)
+
+        verify(reviewService, times(1)).getReview(anyOrNull())
+        verifyNoMoreInteractions(reviewService)
+    }
+
+    @Test
+    fun getReviewByPluginTest() {
+        `when`(reviewService.getReviewByPlugin(anyOrNull())).thenReturn(Flux.just(created))
+
+        testClient.get()
+            .uri("/review/plugin/${UUID.randomUUID()}")
+            .exchange()
+            .expectStatus().isOk
+            .expectBody()
+            .jsonPath("$[0].accountId").isEqualTo(created.accountId.toString())
+            .jsonPath("$[0].pluginId").isEqualTo(created.pluginId.toString())
+            .jsonPath("$[0].rating").isEqualTo(created.rating)
+            .jsonPath("$[0].comment").isEqualTo(created.comment)
+
+        verify(reviewService, times(1)).getReviewByPlugin(anyOrNull())
+        verifyNoMoreInteractions(reviewService)
+    }
+
+    @Test
+    fun getReviewsTest() {
+        `when`(reviewService.getReviews()).thenReturn(Flux.just(created))
+
+        testClient.get()
+            .uri("/review/all")
+            .exchange()
+            .expectStatus().isOk
+            .expectBody()
+            .jsonPath("$[0].accountId").isEqualTo(created.accountId.toString())
+            .jsonPath("$[0].pluginId").isEqualTo(created.pluginId.toString())
+            .jsonPath("$[0].rating").isEqualTo(created.rating)
+            .jsonPath("$[0].comment").isEqualTo(created.comment)
+
+        verify(reviewService, times(1)).getReviews()
+        verifyNoMoreInteractions(reviewService)
+    }
+
+    @Test
+    fun deleteReviewTest() {
+        `when`(reviewService.removeReview(anyOrNull())).thenReturn(Mono.just(created))
+
+        testClient.delete()
+            .uri("/review/${UUID.randomUUID()}")
+            .exchange()
+            .expectStatus().isOk
+            .expectBody()
+            .jsonPath("$.accountId").isEqualTo(created.accountId.toString())
+            .jsonPath("$.pluginId").isEqualTo(created.pluginId.toString())
+            .jsonPath("$.rating").isEqualTo(created.rating)
+            .jsonPath("$.comment").isEqualTo(created.comment)
+
+        verify(reviewService, times(1)).removeReview(anyOrNull())
+        verifyNoMoreInteractions(reviewService)
+    }
+}
