c58584e36dfcb93e486e2e693b86be0803e2c5cc
Joe Matthews <joetymatthews@gmail.com>
2021-08-25 21:56:33 +0100

    refactor: update download link repository

--- src/main/kotlin/com/jtm/minecraft/core/usecase/repository/DownloadLinkRepository.kt
+++ src/main/kotlin/com/jtm/minecraft/core/usecase/repository/DownloadLinkRepository.kt
@@ -3,11 +3,7 @@ package com.jtm.minecraft.core.usecase.repository
 import com.jtm.minecraft.core.domain.entity.DownloadLink
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import org.springframework.stereotype.Repository
-import reactor.core.publisher.Mono
 import java.util.*
 
 @Repository
-interface DownloadLinkRepository: ReactiveMongoRepository<DownloadLink, UUID> {
-
-    fun findByIdAndAccountId(id: UUID, accountId: UUID): Mono<DownloadLink>
-}
+interface DownloadLinkRepository: ReactiveMongoRepository<DownloadLink, UUID>
