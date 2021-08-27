f30e1617c8c87aa95e249213143a4b088a1179dc
Joe Matthews <joetymatthews@gmail.com>
2021-08-27 14:25:39 +0100

    print event to console

--- src/main/kotlin/com/jtm/minecraft/data/service/HookService.kt
+++ src/main/kotlin/com/jtm/minecraft/data/service/HookService.kt
@@ -1,14 +1,11 @@
 package com.jtm.minecraft.data.service
 
-import com.jtm.minecraft.core.domain.exceptions.FailedDeserialization
 import com.jtm.minecraft.core.domain.exceptions.InvalidPaymentIntent
 import com.jtm.minecraft.core.util.UtilString
 import com.jtm.minecraft.data.service.plugin.AccessService
 import com.stripe.model.Event
 import com.stripe.model.PaymentIntent
-import com.stripe.model.StripeObject
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 import java.util.*
@@ -17,6 +14,8 @@ import java.util.*
 class HookService @Autowired constructor(private val accessService: AccessService) {
 
     fun addAccess(event: Event): Mono<Void> {
+        println("Api: ${event.apiVersion}")
+        println(event)
         val dataObjectDeserializer = event.dataObjectDeserializer
         val stripeObject = dataObjectDeserializer.deserializeUnsafe()
         val intent: PaymentIntent = stripeObject as PaymentIntent
