36c2e9a7ffb1736865781eb460334b59b9be1047
Joe Matthews <joetymatthews@gmail.com>
2021-09-01 19:58:25 +0100

    refactor: update plugin intent with default constructor

--- src/main/kotlin/com/jtm/minecraft/core/domain/model/PluginIntent.kt
+++ src/main/kotlin/com/jtm/minecraft/core/domain/model/PluginIntent.kt
@@ -2,4 +2,4 @@ package com.jtm.minecraft.core.domain.model
 
 import java.util.*
 
-data class PluginIntent(val total: Double, val currency: String, val plugins: List<UUID>)
+data class PluginIntent(val total: Double = 0.0, val currency: String = "gbp", val plugins: List<UUID> = listOf())
