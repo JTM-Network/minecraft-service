33dff2de21a7daa6a4aa0a33fadcadc8de08447d
Joe Matthews <joetymatthews@gmail.com>
2022-01-27 11:33:06 +0000

    new: initial profile-service project commit

---
+++ profile-service/src/test/kotlin/com/jtm/profile/ProfileServiceApplicationTestSuite.kt
@@ -0,0 +1,8 @@
+package com.jtm.profile
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(value = [])
+class ProfileServiceApplicationTestSuite
