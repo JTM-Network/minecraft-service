c3ec2a9eae8e7883dced91c4a7a35c74e7e09e95
Joe Matthews <joetymatthews@gmail.com>
2022-02-04 13:55:10 +0000

    refactor: update project files & test suites
    - add profile host to version service yaml
    - update tests of ProfileServiceApplicationTestSuite.kt
    - update tests of VersionApplicationTestSuite.kt

--- profile-service/src/test/kotlin/com/jtm/profile/ProfileServiceApplicationTestSuite.kt
+++ profile-service/src/test/kotlin/com/jtm/profile/ProfileServiceApplicationTestSuite.kt
@@ -1,6 +1,8 @@
 package com.jtm.profile
 
+import com.jtm.profile.data.service.AuthServiceTest
 import com.jtm.profile.data.service.ProfileServiceTest
+import com.jtm.profile.entrypoint.controller.AuthControllerTest
 import com.jtm.profile.entrypoint.controller.ProfileControllerTest
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
@@ -8,6 +10,9 @@ import org.junit.runners.Suite
 @RunWith(Suite::class)
 @Suite.SuiteClasses(value = [
     ProfileServiceTest::class,
-    ProfileControllerTest::class
+    ProfileControllerTest::class,
+
+    AuthServiceTest::class,
+    AuthControllerTest::class
 ])
 class ProfileServiceApplicationTestSuite
--- version-service/src/test/kotlin/com/jtm/version/VersionApplicationTestSuite.kt
+++ version-service/src/test/kotlin/com/jtm/version/VersionApplicationTestSuite.kt
@@ -1,6 +1,8 @@
 package com.jtm.version
 
+import com.jtm.version.data.service.FileSystemServiceTest
 import com.jtm.version.data.service.VersionServiceTest
+import com.jtm.version.entrypoint.controller.FileSystemControllerTest
 import com.jtm.version.entrypoint.controller.VersionControllerTest
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
@@ -8,6 +10,9 @@ import org.junit.runners.Suite
 @RunWith(Suite::class)
 @Suite.SuiteClasses(value = [
     VersionControllerTest::class,
-    VersionServiceTest::class
+    VersionServiceTest::class,
+
+    FileSystemServiceTest::class,
+    FileSystemControllerTest::class
 ])
 class VersionApplicationTestSuite
